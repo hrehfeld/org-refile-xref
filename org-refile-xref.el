049e5e661c79b13dbd3b42aa2da2a514907487dd
Hauke Rehfeld <git@haukerehfeld.de>
2022-04-15 16:08:30 +0200

    cln: adapt names etc

--- org-refile-xref.el
+++ org-refile-xref.el
@@ -1,6 +1,13 @@
 (require 'org)
-      ;; make org-*-goto-last-stored behave well with xref stack
-      (defun my-org-refile-goto-last-stored-xref-push-advice (&rest args)
-        (xref-push-marker-stack))
-      (advice-add #'org-refile-goto-last-stored :before #'my-org-refile-goto-last-stored-xref-push-advice)
-      (advice-add #'org-capture-goto-last-stored :before #'my-org-refile-goto-last-stored-xref-push-advice)
+;; make org-*-goto-last-stored behave well with xref stack
+(defun org-refile-xref-goto-last-stored-xref-push-advice (&rest args)
+  (xref-push-marker-stack))
+(advice-add #'org-refile-goto-last-stored :before #'org-refile-xref-goto-last-stored-xref-push-advice)
+(advice-add #'org-capture-goto-last-stored :before #'org-refile-xref-goto-last-stored-xref-push-advice)
+
+(defun org-refile-xref-remove-advice ()
+  (interactive)
+  (advice-remove #'org-refile-goto-last-stored #'org-refile-xref-goto-last-stored-xref-push-advice)
+  (advice-remove #'org-capture-goto-last-stored #'org-refile-xref-goto-last-stored-xref-push-advice))
+
+(provide 'org-refile-xref)
