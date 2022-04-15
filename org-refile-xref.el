3a4c39d0b548a1f795cb58b37540ab7ec44c1ded
Hauke Rehfeld <github@haukerehfeld.de>
2022-04-15 16:06:09 +0200

    Update org-refile-xref.el
    add: initial

--- org-refile-xref.el
+++ org-refile-xref.el
@@ -1,1 +1,6 @@
-
+(require 'org)
+      ;; make org-*-goto-last-stored behave well with xref stack
+      (defun my-org-refile-goto-last-stored-xref-push-advice (&rest args)
+        (xref-push-marker-stack))
+      (advice-add #'org-refile-goto-last-stored :before #'my-org-refile-goto-last-stored-xref-push-advice)
+      (advice-add #'org-capture-goto-last-stored :before #'my-org-refile-goto-last-stored-xref-push-advice)
